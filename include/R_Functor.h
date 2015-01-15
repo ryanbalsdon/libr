101a6e91d4e2ce785212490d74206d72171f5ba8
Ryan Balsdon <ryan.balsdon@shopify.com>
2015-01-14 23:23:43 -0500

    Renamed R_Functor

---
+++ include/R_Functor.h
@@ -0,0 +1,24 @@
+#ifndef R_Functor_h
+#define R_Functor_h
+
+/* Author: Ryan Balsdon <ryanbalsdon@gmail.com>
+ *  I dedicate any and all copyright interest in this software to the
+ * public domain. I make this dedication for the benefit of the public at
+ * large and to the detriment of my heirs and successors. I intend this
+ * dedication to be an overt act of relinquishment in perpetuity of all
+ * present and future rights to this software under copyright law.
+ */
+
+#include "R_Type.h"
+
+typedef void (*R_Functor_Function)(void* state);
+typedef struct {
+	R_Type* type;
+	void* state; //Must be an instance of an R_Type object because R_Type_Delete will be called against it
+	R_Functor_Function function;
+} R_Functor;
+R_Type_Declare(R_Functor);
+
+void* R_Functor_call(R_Functor*); //Returns the functor's state object
+
+#endif /*R_Functor_h*/
---
+++ test/R_Functor_test.c
@@ -0,0 +1,47 @@
+/* Author: Ryan Balsdon <ryanbalsdon@gmail.com>
+ *  I dedicate any and all copyright interest in this software to the
+ * public domain. I make this dedication for the benefit of the public at
+ * large and to the detriment of my heirs and successors. I intend this
+ * dedication to be an overt act of relinquishment in perpetuity of all
+ * present and future rights to this software under copyright law.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "R_Functor.h"
+#include "R_ObjectArray.h"
+
+void Integer_Doubler(R_Integer* integer) {
+	integer->Integer *= 2;
+}
+R_Functor* Integer_Doubler_Functor_Init(R_Functor* functor, int initial) {
+	R_Integer* state = R_Type_New(R_Integer);
+	state->Integer = initial;
+	functor->state = state;
+	functor->function = (R_Functor_Function)Integer_Doubler;
+	return functor;
+}
+
+void test_functors(void) {
+	R_Functor* functor_6 = R_Type_New(R_Functor);
+	Integer_Doubler_Functor_Init(functor_6, 6);
+	R_Integer* value = R_Functor_call(functor_6);
+	assert(value->Integer == 12);
+	value = R_Functor_call(functor_6);
+	assert(value->Integer == 24);
+	R_Type_Delete(functor_6);
+
+	R_Functor* functor_10 = Integer_Doubler_Functor_Init(R_Type_New(R_Functor), 100);
+	value = R_Functor_call(functor_10);
+	assert(value->Integer == 200);
+	R_Type_Delete(functor_10);
+}
+
+int main(void) {
+  test_functors();
+
+  assert(R_Type_BytesAllocated == 0);
+  printf("Pass\n");
+}
