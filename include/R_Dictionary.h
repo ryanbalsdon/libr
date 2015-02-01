1b54ba4322ae98fca25d862acd61ae41d8ff4404
Ryan Balsdon <ryan.balsdon@shopify.com>
2015-01-31 23:59:43 -0500

    a bit more documentation

--- include/R_Dictionary.h
+++ include/R_Dictionary.h
@@ -16,20 +16,64 @@
 typedef struct R_Dictionary R_Dictionary;
 R_Type_Declare(R_Dictionary);
 
+/*  R_Dictionary_addObjectOfType
+    Allocates a new object with the given R_Type definition and adds it to the dictionary.
+ */
 void* R_Dictionary_addObjectOfType(R_Dictionary* self, const char* key, const R_Type* type);
+
+/*  R_Dictionary_add
+    Allocates a new object with the given R_Type type name and adds it to the dictionary.
+ */
 #define R_Dictionary_add(self, key, Type) (Type*)R_Dictionary_addObjectOfType(self, key, R_Type_Object(Type))
+
+/*  R_Dictionary_addCopy
+    Copies the given object and adds it to the dictionary. If the object doesn't have an R_Type_Copier, this will fail.
+ */
 void* R_Dictionary_addCopy(R_Dictionary* self, const char* key, const void* object);
+
+/*  R_Dictionary_transferOwnership
+    Adds the pointer to the dictionary without copying the object. This is dangerous. The object must be on the heap.
+ */
 void* R_Dictionary_transferOwnership(R_Dictionary* self, const char* key, void* object);
+
+/*  R_Dictionary_remove
+    Removes the object with the given key.
+ */
 void R_Dictionary_remove(R_Dictionary* self, const char* key);
+
+/*  R_Dictionary_get
+    Fetches the object with the given key. Returns NULL if it doesn't exist.
+ */
 void* R_Dictionary_get(R_Dictionary* self, const char* key);
 
+/*  R_Dictionary_removeAll
+    Removes all objects from the dictionary.
+ */
 void R_Dictionary_removeAll(R_Dictionary* self);
+
+/*  R_Dictionary_isPresent
+    Returns true if the key exists in the dictionary or false if it isn't.
+ */
 bool R_Dictionary_isPresent(R_Dictionary* self, const char* key);
+
+/*  R_Dictionary_isNotPresent
+    Returns false if the key exists in the dictionary or true if it isn't.
+ */
 bool R_Dictionary_isNotPresent(R_Dictionary* self, const char* key);
 
+/*  R_Dictionary_size
+    Returns the number of keys in the dictionary.
+ */
 size_t R_Dictionary_size(R_Dictionary* self);
 
+/*  R_Dictionary_toJson
+    Writes the dictionary to the given string, as json. 
+ */
 R_String* R_Dictionary_toJson(R_Dictionary* self, R_String* buffer);
+
+/*  R_Dictionary_toJson
+    Initializes the dictionary with the given json string.
+ */
 R_Dictionary* R_Dictionary_fromJson(R_Dictionary* self, R_String* buffer);
 
 #endif /* R_Dictionary_h */
